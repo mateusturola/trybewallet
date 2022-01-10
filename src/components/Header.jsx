6b619b43233f41716bba2089fe5930fb2ab5bc13
mateusturola <turolamateus@gmail.com>
2022-01-10 17:41:28 -0300

    feat: Add Header com informações sobre o usuário

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header>
+        <div className="user-email" data-testid="email-field">
+          {email}
+        </div>
+        <div className="total-field" data-testid="total-field">
+          0
+        </div>
+        <div className="currency-field" data-testid="header-currency-field">
+          BRL
+        </div>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Header);
