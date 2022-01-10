cc9dce337856d0f03b19d183e333fa14ae6f81ac
mateusturola <turolamateus@gmail.com>
2022-01-10 16:09:42 -0300

    feat: ADD Redux

---
+++ src/reducers/user.js
@@ -0,0 +1,6 @@
+// Esse reducer será responsável por tratar as informações da pessoa usuária
+export const INITIAL_STATE = {
+  email: '',
+};
+
+export const user = (state = INITIAL_STATE) => state;
---
+++ src/reducers/wallet.js
@@ -0,0 +1,6 @@
+export const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+export const wallet = (state = INITIAL_STATE) => state;
